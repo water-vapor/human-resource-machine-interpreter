7cbaa7208a94b619af683a162d06f6fbc61939b1
yuwanzeon <zzhaoah@ust.hk>
2017-01-17 21:52:11 +0800

    implemented interpreter

---
+++ hrm-interpreter.py
@@ -0,0 +1,137 @@
+import glob
+import itertools
+import re
+
+
+def hmri(raw_code, inputs=[], memory_size=16, memory={}):
+
+    # helper function
+    def read_address(raw_addr):
+        if raw_addr[-1] == ']':
+            point_to = int(raw_addr[1:-1])
+            assert point_to < memory_size
+            assert point_to in memory
+            address = memory[point_to]
+        else:
+            address = int(raw_addr)
+        assert address < memory_size
+        return address
+
+    # check head
+    assert raw_code[0] == '-- HUMAN RESOURCE MACHINE PROGRAM --'
+    assert raw_code[1] == ''
+    # remove tail comments and spaces
+    code = list(itertools.takewhile(lambda x: x != '', raw_code[2:]))
+
+    program_length = len(code)
+    program_counter = 0
+    program_steps = 0
+    program_finished = program_counter >= program_length
+
+    # mark jump labels
+    label_pattern = re.compile('.+:')
+    labels = {}
+    for i in range(program_length):
+        if label_pattern.fullmatch(code[i]) is not None:
+            labels[code[i][:-1]] = i
+
+    # comment lines
+    comment_pattern = re.compile("comment.+")
+    comment_counts = 0
+    for i in range(program_length):
+        if comment_pattern.fullmatch(code[i]) is not None:
+            comment_counts += 1
+
+    program_size = program_length - len(labels) - comment_counts
+    current_value = None
+    outputs = []
+    while not program_finished:
+        current_line = code[program_counter].split()
+
+        if current_line[0] == 'INBOX':
+            # the end of infinite loop program in HRM
+            if inputs == []:
+                break
+            current_value = inputs.pop(0)
+            program_counter += 1
+            program_steps += 1
+
+        elif current_line[0] == 'OUTBOX':
+            # cannot output an empty value
+            assert current_value is not None
+            outputs.insert(0, current_value)
+            current_value = None
+            program_counter += 1
+            program_steps += 1
+
+        elif current_line[0] == 'COPYFROM':
+            address = read_address(current_line[1])
+            assert address in memory
+            current_value = memory[address]
+            program_counter += 1
+            program_steps += 1
+
+        elif current_line[0] == 'COPYTO':
+            assert current_value is not None
+            address = read_address(current_line[1])
+            memory[address] = current_value
+            program_counter += 1
+            program_steps += 1
+
+        elif current_line[0] == 'ADD':
+            assert current_value is not None
+            address = read_address(current_line[1])
+            current_value = current_value + memory[address]
+            program_counter += 1
+            program_steps += 1
+
+        elif current_line[0] == 'SUB':
+            assert current_value is not None
+            address = read_address(current_line[1])
+            current_value = current_value - memory[address]
+            program_counter += 1
+            program_steps += 1
+
+        elif current_line[0] == 'BUMPUP':
+            address = read_address(current_line[1])
+            memory[address] += 1
+            current_value = memory[address]
+            program_counter += 1
+            program_steps += 1
+
+        elif current_line[0] == 'BUMPDN':
+            address = read_address(current_line[1])
+            memory[address] -= 1
+            current_value = memory[address]
+            program_counter += 1
+            program_steps += 1
+
+        elif current_line[0] == 'JUMP':
+            jump_label = current_line[1]
+            program_counter = labels[jump_label]
+            program_steps += 1
+
+        elif current_line[0] == 'JUMPZ':
+            assert current_value is not None
+            jump_label = current_line[1]
+            if current_value == 0:
+                program_counter = labels[jump_label]
+                program_steps += 1
+            else:
+                program_counter += 1
+
+        elif current_line[0] == 'JUMPN':
+            assert current_value is not None
+            jump_label = current_line[1]
+            if current_value < 0:
+                program_counter = labels[jump_label]
+                program_steps += 1
+            else:
+                program_counter += 1
+
+        else:
+            program_counter += 1
+
+        program_finished = program_counter >= program_length
+
+    return [outputs, program_steps, program_size]
